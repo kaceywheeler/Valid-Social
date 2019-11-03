6d2cb5dc58639cda2073243e871a28e11db0c8d9
kaceywheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 13:31:31 -0700

    Update SocialRunner.java

--- SocialRunner.java
+++ SocialRunner.java
@@ -1,17 +1,23 @@
 package ifs;
+//Import scanner
 import java.util.Scanner;
+//Import social class
 import ifs.Social;
 
 public class SocialRunner {
 	public static void main (String args[] ) {
 	
+	//Create new scanner
 	Scanner keyboard = new Scanner(System.in);
 	
+	//Get user input for social security number
 	System.out.println("Enter a social security number: ");
 	String social = keyboard.next();
 	
+	//Create new social object
 	Social ref = new Social();
 	
+	//Run new method and print output
 	System.out.println(ref.Social_Check(social));
 	
 	
