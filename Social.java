90a23532d65ce2c651e8f6153270260326eef94c
kaceywheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 13:26:35 -0700

    Update Social.java

--- Social.java
+++ Social.java
@@ -8,11 +8,14 @@ public class Social {
 		char hyphen_6 = social.charAt(6);
 		char hyphen = '-';
 		
+		//Social valid if hyphen at 3 and 6
 		if (hyphen_3 == hyphen && hyphen_6 == hyphen) {
 			int len = social.length();
+			//Print last 4 digits of number
 			output = social.substring(len - 4, len);
 		}
 		
+		//If not valid, output is bad
 		else {
 			output = "bad";
 		
